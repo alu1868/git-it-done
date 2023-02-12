2e80c088696db323ebf93d302b819cbf7198b6b8
Alex <alu1868@gmail.com>
2023-02-12 17:42:53 -0600

    set up to receive promise, then display json if successful

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(users) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + users + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    // returns promise --> .then() is a function in case the promise returns resolved
+    response.json().then(function(data) {
+      // display json data
+      console.log(data)
+    });
+  });
 };
 
 getUserRepos();
